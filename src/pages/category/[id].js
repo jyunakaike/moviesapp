4473f4de235a548c800ef32643b8ff8cae43219b
jyun <you@example.com>
2022-05-25 18:43:57 -0400

    feat: validation categories

--- src/pages/category/[id].js
+++ src/pages/category/[id].js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Error from 'next/error';
 import axios from 'axios';
 import { useGetMoviesByCategory } from 'hooks/useGetMoviesByCategory';
+import { getCategoriesPreview } from 'hooks/useGetCategoriesPreview';
+
 import { MovieContainer } from 'components/MovieContainer';
 
 const CategoryItem = () => {
     const {
         query: { id },
     } = useRouter();
-
     const [categoryId, categoryName] = []
-
     if (id) {
         [categoryId, categoryName] = id.split('-');
+
     }
 
     const api = axios.create({
@@ -27,8 +29,18 @@ const CategoryItem = () => {
 
     const movies = useGetMoviesByCategory(api, categoryId);
 
+    const validate = getCategoriesPreview(api)
+    if (validate) {
+        const validate2 = validate.genres
+            .some(movie => movie.id.toString() === categoryId && movie.name === categoryName)
+        if (!validate2) {
+            return <Error statusCode={404} />
+        }
+    }
+
     return (
         <div style={{ margin: "0.1rem" }} >
+            {categoryId}
             <h1 style={{ marginBottom: "1rem" }} >{categoryName}</h1>
             {
                 (movies)
